1d089cebaa9c9defa76939b34c471a95edef5c5b
yoohun <317043855@qq.com>
2018-12-05 10:03:16 +0800

    城市名的渲染以及better-scroll的设置

--- src/pages/city/city.vue
+++ src/pages/city/city.vue
@@ -2,12 +2,15 @@
   <div>
     <cityheader></cityheader>
     <citysearch></citysearch>
+    <citylist :cities="cities"></citylist>
   </div>
 </template>
 
 <script>
 import cityheader from './components/cityheader'
 import citysearch from './components/citysearch'
+import citylist from './components/citylist'
+import axios from 'axios'
 export default {
   name: 'city',
   data () {
@@ -17,7 +20,23 @@ export default {
   },
   components: {
     cityheader,
-    citysearch
+    citysearch,
+    citylist
+  },
+  methods: {
+    getInfor () {
+      axios.get('/api/city.json')
+        .then(this.showInfor)
+    },
+    showInfor (res) {
+      res = res.data
+      if (res.ret && res.data) {
+        this.cities = res.data.cities
+      }
+    }
+  },
+  mounted () {
+    this.getInfor()
   }
 }
 </script>
---
+++ src/pages/city/components/citylist.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="citylist" ref="wrapper">
+    <div>
+      <div class="area" v-for="(item, key) of cities" :key="key">
+        <div class="cityIndex">{{key}}</div>
+        <div class="cityName border-bottom" v-for="itemlist of item" :key="itemlist.id" @click="clickLetter(itemlist.name)">{{itemlist.name}}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import BScroll from 'better-scroll'
+export default {
+  name: 'citylist',
+  props: {
+    cities: Object
+  },
+  methods: {
+    clickLetter (name) {
+      console.log(name)
+    }
+  },
+  mounted () {
+    this.scroll = new BScroll(this.$refs.wrapper, {
+      click: true
+    })
+    console.log(this.cities)
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '../../../assets/style/varibles.styl'
+  .border-topbottom
+    &:before
+      border-color: #ccc
+    &:after
+      border-color: #ccc
+  .border-bottom
+    &:before
+      border-color: #ccc
+  .citylist
+    position absolute
+    top 1.44rem
+    left 0
+    right 0
+    bottom 0
+    overflow hidden
+    .cityIndex
+      line-height .5rem
+      background #e0e0e0
+      color #000000
+      padding 0 .2rem
+      font-size .32rem
+    .cityName
+      padding 0 .2rem
+      line-height .6rem
+      font-size .3rem
+</style>
