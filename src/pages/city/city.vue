c03df1cba3c00d28a58d07357d19fd0a1bc3d192
yoohun <317043855@qq.com>
2018-12-05 08:22:24 +0800

    搜索框 样式

--- src/pages/city/city.vue
+++ src/pages/city/city.vue
@@ -1,15 +1,23 @@
 <template>
   <div>
     <cityheader></cityheader>
+    <citysearch></citysearch>
   </div>
 </template>
 
 <script>
 import cityheader from './components/cityheader'
+import citysearch from './components/citysearch'
 export default {
   name: 'city',
+  data () {
+    return {
+      cities: {}
+    }
+  },
   components: {
-    cityheader
+    cityheader,
+    citysearch
   }
 }
 </script>
---
+++ src/pages/city/components/citysearch.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div class="search">
+      <input type="text" placeholder="输入相应的城市或拼音" class="searchput" v-model="cityname">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'citysearch',
+  data () {
+    return {
+      cityname: ''
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '../../../assets/style/varibles.styl'
+  .search
+    height .64rem
+    background $bgColor
+    padding-left .2rem
+    padding-right  .2rem
+    .searchput
+      width 100%
+      height .58rem
+      line-height .58rem
+      text-align center
+      border-radius .05rem
+      padding 0 .1rem
+      box-sizing border-box
+      color #666
+</style>
